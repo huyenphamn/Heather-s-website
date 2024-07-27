e988006210bc41371dfb946164713cbe9ebc88f7
Heather Pham <phamngochuyen1911@gmail.com>
2024-07-27 13:03:29 +0700

    Add menu items' titles

--- heatherpham/src/components/Navbar.jsx
+++ heatherpham/src/components/Navbar.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import styles from "./Navbar.module.css"
+
 export const Navbar = () => {
     return(
         <div>
-            <nav>
-                <a href="/">Portfolio</a>
+            <nav className={styles.navbar}>
+                <a className={styles.title} href="/">Portfolio</a>
+                <div className={styles.menu}>
+                    <ul className={styles.menuItems}>
+                        <li>
+                            <a href="#about">About</a>
+                        </li>
+                        <li>
+                            <a href="#experience">Experience</a>
+                        </li>
+                        <li>
+                            <a href="#projects">Projects</a>
+                        </li>
+                        <li>
+                            <a href="#activity">Activity</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </div>
     );
