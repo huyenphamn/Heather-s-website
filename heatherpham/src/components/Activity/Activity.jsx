0507c265561e888a8ddf28632865a4470007014e
Heather Pham <phamngochuyen1911@gmail.com>
2024-07-27 17:58:09 +0700

    Update Activity

--- heatherpham/src/components/Activity/Activity.jsx
+++ heatherpham/src/components/Activity/Activity.jsx
@@ -1,20 +1,32 @@
 import React from "react"
-import activity from "../../data/activityOrg.json"
+import activity from "../../data/activity.json"
+import styles from "./Activity.module.css"
+import { getImageUrl } from "../../util";
 
 export const Activity = () => {
-    return (<section id="activity">
-        <h2>Activity</h2>
-        <div>
-            <div>
+    return (<section id="activity" className={styles.container}>
+        <h2 className={styles.title}>Activity</h2>
+        <div className={styles.content}>
+            <ul>
                 {
                     activity.map((activity, id) => {
-                        return <div>
-                            
-                        </div>
+                        return <li key={id}>
+                            <div><img src={getImageUrl(activity.imageSrc)} alt={`${activity.organisation} Logo`}/>
+                                <h3>{`${activity.role}, ${activity.organisation}`}</h3>
+                                <p>
+                                {`${activity.startDate} - ${activity.endDate}`}
+                                </p>
+                                <ul>
+                                {
+                    activity.experiences.map((experience, id) => {
+                        return <li key={id}>{experience}</li>})
+                            }
+                                </ul>
+                            </div>
+                        </li>
                     })
                 }
-            </div>
-            <ul></ul>
+            </ul>
         </div>
     </section>);
 };
